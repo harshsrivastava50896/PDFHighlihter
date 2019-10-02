c9deb8c59cb30faa5360d9f498e563d1d8e3ec86
Harsh Srivastava <Harsh.Srivastava@neudesic.com>
2019-10-02 15:06:27 +0530

    Inital commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# NgPdfHighlighter
+
+This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 7.0.2.
+
+
+## Demo page
+
+[https://kbatyrbayev.github.io/ng-pdf-highlighter/](https://kbatyrbayev.github.io/ng-pdf-highlighter/)
+
+## Overview
+Used a library  [ng2-pdf-viewer](https://github.com/VadimDez/ng2-pdf-viewer) to show pdf 
+
+## Installation
+
+```sh
+$ git clone https://github.com/kbatyrbayev/ng-pdf-highlighter.git
+$ cd ng-pdf-highlighter
+$ npm install
+$ ng serve --open
+```
---
+++ src/app/app.component.ts
@@ -0,0 +1,349 @@
+import { Component } from '@angular/core';
+import * as Popper from 'popper.js/dist/umd/popper.js';
+import * as $ from 'jquery';
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  constructor() {
+    setTimeout(() => {
+      this.pageCoordinates = document
+        .getElementById('page1')
+        .getBoundingClientRect();
+      this.setpixels();
+      console.log('area gage', this.pageCoordinates);
+    }, 1000);
+
+    this.areaInfo = [
+      {
+        rectangleId: 'rectangle-1',
+        pageNumber: 1,
+        rect: {
+          height: 0.009845423512160778,
+          width: 0.061697207391262054,
+          x1: 0.24041353166103363,
+          x2: 0.3021107316017151,
+          y1: 0.044183917343616486,
+          y2: 0.05402934178709984
+        },
+        Id: '2463cd45-9f24-4c39-97c7-95c86acf4a2c',
+        isDelete: false,
+        Text: 'Department'
+      },
+      {
+        rectangleId: 'rectangle-2',
+        pageNumber: 1,
+        rect: {
+          height: 0.011163070797920227,
+          width: 0.05307428166270256,
+          x1: 0.36285918951034546,
+          x2: 0.4159334897994995,
+          y1: 0.09659016877412796,
+          y2: 0.10775323957204819
+        },
+        Id: '24211a34-379c-4cab-82f8-cfbd8d6f2fe8',
+        isDelete: false,
+        Text: 'transfer'
+      }
+    ];
+  }
+  rect: Rectangle = { x1: 0, y1: 0, x2: 0, y2: 0, width: 0, height: 0 };
+  lastMousePosition: Position = { x: 0, y: 0 };
+  canvasPosition: Position = { x: 0, y: 0 };
+  mousePosition: Position = { x: 0, y: 0 };
+  mouseDownFlag = false;
+  pagePosition: Position = { x: 0, y: 0 };
+  pageCoordinates: any = {
+    height: 0,
+    width: 0,
+    x: 0,
+    y: 0,
+    bottom: 0,
+    top: 0,
+    left: 0,
+    right: 0
+  };
+  cnv;
+  pdfBody;
+  ctx;
+  element = null;
+  dataPageNumber: number;
+
+  areaInfo: AreaInfo[] = [];
+  areaInfoInPixels: AreaInfo[] = [];
+  onPageResize: number;
+  onPageLoad: number;
+  // added new div with rects when pages rendered
+  indexOfPage = 1;
+
+  showPopup = false;
+  extractedData: string[] = [];
+  listRectangleId = '';
+  onResize(event) {
+    this.setpixels();
+    const image = document.getElementById('page1');
+    console.log('area info', image.getBoundingClientRect());
+  }
+
+  setpixels() {
+    this.areaInfoInPixels = [] = [];
+    this.areaInfo.forEach((x) => {
+      const areaCooradinate: AreaInfo = new AreaInfo();
+      areaCooradinate.rect.height = x.rect.height * this.pageCoordinates.height;
+      areaCooradinate.rect.width = x.rect.width * this.pageCoordinates.width;
+      areaCooradinate.rect.x1 = x.rect.x1 * this.pageCoordinates.width;
+      areaCooradinate.rect.x2 = x.rect.x2 * this.pageCoordinates.width;
+      areaCooradinate.rect.y1 = x.rect.y1 * this.pageCoordinates.height;
+      areaCooradinate.rect.y2 = x.rect.y2 * this.pageCoordinates.height;
+      areaCooradinate.pageNumber = x.pageNumber;
+      areaCooradinate.Id = x.Id;
+      areaCooradinate.rectangleId = x.rectangleId;
+      this.areaInfoInPixels.push(areaCooradinate);
+      this.extractedData.push(x.Text);
+    });
+    console.log('Area Info in pixels', this.areaInfoInPixels);
+    console.log('extracted text', this.extractedData);
+    
+  }
+  mouseEvent(event) {
+    if (!this.showPopup) {
+      if (event.type === 'mousemove') {
+        this.mousePosition = {
+          x: event.clientX - this.pagePosition.x,
+          y: event.clientY - this.pagePosition.y
+        };
+
+        if (this.mouseDownFlag) {
+          const width = this.mousePosition.x - this.lastMousePosition.x;
+          const height = this.mousePosition.y - this.lastMousePosition.y;
+          this.rect = {
+            x1: this.lastMousePosition.x,
+            y1: this.lastMousePosition.y,
+            x2: this.mousePosition.x,
+            y2: this.mousePosition.y,
+            width: width,
+            height: height
+          };
+
+          if (this.element != null) {
+            this.element.style.width = width + 'px';
+            this.element.style.height = height + 'px';
+            if (this.rect.width > 0 && this.rect.height > 0) {
+              document
+                .getElementsByClassName('to-draw-rectangle')
+                [this.dataPageNumber - 1].appendChild(this.element);
+            }
+          }
+        }
+      }
+
+      if (event.type === 'mousedown') {
+        this.mouseDownFlag = true;
+        const path = this.composedPath(event.target);
+        const eventPath = path.find((p) => p.className == 'page');
+
+        if (typeof eventPath !== 'undefined') {
+          this.dataPageNumber = parseInt(
+            eventPath.getAttribute('data-page-number')
+          ); // get id of page
+          const toDrawRectangle = document.getElementsByClassName(
+            'to-draw-rectangle'
+          );
+          const pageOffset = toDrawRectangle[
+            this.dataPageNumber - 1
+          ].getBoundingClientRect();
+          this.pagePosition = {
+            x: pageOffset.left,
+            y: pageOffset.top
+          };
+
+          this.lastMousePosition = {
+            x: event.clientX - this.pagePosition.x,
+            y: event.clientY - this.pagePosition.y
+          };
+
+          const rectId =
+            document.getElementsByClassName('rectangle').length + 1;
+          this.element = document.createElement('div');
+          this.element.className = 'rectangle';
+          this.element.id = 'rectangle-' + rectId;
+          this.element.style.position = 'absolute';
+          this.element.style.border = '2px solid #0084FF';
+          this.element.style.borderRadius = '3px';
+          this.element.style.left = this.lastMousePosition.x + 'px';
+          this.element.style.top = this.lastMousePosition.y + 'px';
+        }
+      }
+
+      if (event.type === 'mouseup') {
+        this.mouseDownFlag = false;
+
+        if (this.rect.height > 0 && this.rect.width > 0) {
+          const popper = document.querySelector('.js-popper');
+          new Popper(this.element, popper, {
+            placement: 'top-end'
+          });
+          this.showPopup = true;
+        }
+      }
+    }
+  }
+  pageRendered(event) {
+    const elem = document.createElement('div');
+    elem.className = 'to-draw-rectangle';
+    elem.style.position = 'absolute';
+    elem.style.left = 0 + 'px';
+    elem.style.top = 0 + 'px';
+    elem.style.right = 0 + 'px';
+    elem.style.bottom = 0 + 'px';
+    elem.style.cursor = 'crosshair';
+    // elem.style.background = 'red';
+    // elem.style.opacity = '0.4';
+    const path = this.composedPath(event.target);
+
+    path.find((p) => p.className == 'page').appendChild(elem);
+
+    $('.textLayer').addClass('disable-textLayer');
+
+    // const rectElem = this.areaInfo.find(
+    //   (f) => f.pageNumber === this.indexOfPage
+    // );
+
+    this.areaInfoInPixels.forEach((x) => {
+      if (x.pageNumber == this.indexOfPage) {
+        if (typeof x !== undefined) {
+          const rectId =
+            document.getElementsByClassName('rectangle').length + 1;
+          const rect = document.createElement('div');
+          rect.className = 'rectangle';
+          rect.id = 'rectangle-' + rectId;
+          rect.style.position = 'absolute';
+          rect.style.border = '2px solid #0084FF';
+          rect.style.borderRadius = '3px';
+          rect.style.left = x.rect.x1 + 'px';
+          rect.style.top = x.rect.y1 + 'px';
+          rect.style.width = x.rect.width + 'px';
+          rect.style.height = x.rect.height + 'px';
+          rect.style.cursor = 'pointer';
+          // get to-draw-rectangle div and add rectangle
+          path.find((p) => p.className == 'page').children[2].appendChild(rect);
+        }
+      }
+    });
+    // if (typeof rectElem !== 'undefined') {
+    //   const rectId = document.getElementsByClassName('rectangle').length + 1;
+    //   const rect = document.createElement('div');
+    //   rect.className = 'rectangle';
+    //   rect.id = 'rectangle-' + rectId;
+    //   rect.style.position = 'absolute';
+    //   rect.style.border = '2px solid #0084FF';
+    //   rect.style.borderRadius = '3px';
+    //   rect.style.left = rectElem.rect.x1 + 'px';
+    //   rect.style.top = rectElem.rect.y1 + 'px';
+    //   rect.style.width = rectElem.rect.width + 'px';
+    //   rect.style.height = rectElem.rect.height + 'px';
+    //   rect.style.cursor = 'pointer';
+    //   // get to-draw-rectangle div and add rectangle
+    //   path.find((p) => p.className == 'page').children[2].appendChild(rect);
+    // }
+    this.indexOfPage++;
+  }
+
+  composedPath(el) {
+    const path = [];
+    while (el) {
+      path.push(el);
+      if (el.tagName === 'HTML') {
+        path.push(document);
+        path.push(window);
+        return path;
+      }
+      el = el.parentElement;
+    }
+  }
+  getStyle() {
+    if (this.showPopup) {
+      return 'block';
+    } else {
+      return 'none';
+    }
+  }
+
+  save() {
+    this.areaInfo.push({
+      rectangleId: this.element.id,
+      pageNumber: this.dataPageNumber,
+      rect: this.rect,
+      isDelete: false
+    });
+    this.showPopup = false;
+    this.rect = { x1: 0, y1: 0, x2: 0, y2: 0, width: 0, height: 0 };
+  }
+
+  cancel() {
+    const rectId = this.element.getAttribute('id');
+    $('#' + rectId).remove();
+    this.showPopup = false;
+    this.rect = { x1: 0, y1: 0, x2: 0, y2: 0, width: 0, height: 0 };
+  }
+
+  delete(list: AreaInfo) {
+    document.getElementById(list.rectangleId).remove();
+    this.areaInfo.find(
+      (f) => f.rectangleId === list.rectangleId
+    ).isDelete = true;
+    this.areaInfo = this.areaInfo.filter((f) => f.isDelete === false);
+  }
+  moveTo(list: AreaInfo) {
+    if (this.listRectangleId != '') {
+      if (document.getElementById(this.listRectangleId)) {
+        document.getElementById(this.listRectangleId).style.background =
+          'transparent';
+        document.getElementById(this.listRectangleId).style.opacity = '1';
+      }
+    }
+    if (this.listRectangleId !== list.rectangleId) {
+      document
+        .getElementById(list.rectangleId)
+        .scrollIntoView({ block: 'start', behavior: 'smooth' });
+      document.getElementById(list.rectangleId).style.background = 'red';
+      document.getElementById(list.rectangleId).style.opacity = '0.4';
+      this.listRectangleId = list.rectangleId;
+    }
+  }
+}
+
+class Position {
+  x: number;
+  y: number;
+}
+
+class Rectangle {
+  x1: number;
+  y1: number;
+  x2: number;
+  y2: number;
+  width: number;
+  height: number;
+}
+
+class AreaInfo {
+  rectangleId: string;
+  pageNumber: number;
+  rect: Rectangle = new Rectangle();
+  isDelete?: boolean;
+  Text?: string;
+  Id?: string;
+}
+class PagePixels {
+  bottom: number;
+  height: number;
+  left: number;
+  right: number;
+  top: number;
+  width: number;
+  x: number;
+  y: number;
+}
